35d5968712b13e95f2677cd8628b6fa0bb05918c
oxcz <cnzmz@foxmail.com>
2020-03-16 00:16:36 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,10 @@ build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
-    "type": "local",
-    "path": "../Galileo"
+    "name": "Prism",
+    "type": "git",
+    "url": "https://github.com/Reedo0910/Maverick-Theme-Prism.git",
+    "branch": "deploy"
 }
 enable_jsdelivr = {
     "enabled": False,
